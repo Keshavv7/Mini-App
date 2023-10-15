5a45d77fee2f6c0f0eae2b72aec4619d1a37753d
Keshavv7 <cs21b027@iittp.ac.in>
2023-10-15 15:46:53 +0530

    Minor change

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StyleSheet, View, StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import Button from './src/Button'
+// import Button from './src/Button'
 
 export default function App() {
   return (
     <SafeAreaProvider style={styles.wrapper}>
       <View style={styles.container}>
-        <Button />
+        <View style={styles.view1}></View>
       </View>
     </SafeAreaProvider>
   );
@@ -24,6 +24,12 @@ const styles = StyleSheet.create({
     alignContent: 'center',
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  view1: {
+    height: 100,
+    width: 100,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
 
